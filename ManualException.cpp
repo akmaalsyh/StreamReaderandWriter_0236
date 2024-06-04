43958b6db8cb545601cefdc2a66b2355e12d8d4e
akmaalsyh <taufansyah50@gmail.com>
2024-06-04 15:18:42 +0700

    memasukkan int main dan perintah dalam try

--- ManualException.cpp
+++ ManualException.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try
+    {
+        cout << "Selamat belajar di Prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan dieksekusi" << endl;
+    }
+}
