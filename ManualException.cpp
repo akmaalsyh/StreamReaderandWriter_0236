e47e05fa54d2be1561f2a0b1a4a17c0648a064e1
akmaalsyh <taufansyah50@gmail.com>
2024-06-04 15:23:19 +0700

    memasukkan perintah dalam catch

--- ManualException.cpp
+++ ManualException.cpp
@@ -9,4 +9,18 @@ int main()
         throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
     }
+
+    catch(int a)
+    {
+        // blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+
+    catch(...)
+    {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+
+    return 0;
 }
