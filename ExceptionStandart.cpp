4bc33a09c187a78b1347e82604b61ef8f1fe3b6d
akmaalsyh <taufansyah50@gmail.com>
2024-06-04 15:11:26 +0700

    memasukkan perintah dalam catch exception

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -15,4 +15,11 @@ int main ()
         cout << data.at(5) << endl;
         // memanggil array elemen ke 5
     }
+
+    catch (exception& e)
+    {
+        // penangkap menggunakan obyek exception
+        cout << e.what() << endl;
+        /*akan dieksekusi karna array data hanya memiliki 3 elemen*/
+    }
 }
