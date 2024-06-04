e3d03bdd660a3c99b1222d3b81942cda208ca9d0
akmaalsyh <taufansyah50@gmail.com>
2024-06-04 15:02:20 +0700

    mengganti perintah contohfile menjadi namafile

--- iofilewithPath.cpp
+++ iofilewithPath.cpp
@@ -36,7 +36,7 @@ int main ()
     // membuka file dalam mode membaca
     ifstream infile;
     // menunjuk ke sebuah file
-    infile.open("ContohFile.txt");
+    infile.open(NamaFile + ".txt", ios::out);
 
     cout << endl << ">= Membuka dan membaca file " << endl;
     // jika file ada maka
