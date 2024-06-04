29058dcbecb50387fec7988c48b47735a1386db4
akmaalsyh <taufansyah50@gmail.com>
2024-06-04 14:38:44 +0700

    memasukkan int main dan perintah membuka file dalam mode menulis & menunjuk ke sebuah nama file

--- iofile.cpp
+++ iofile.cpp
@@ -2,3 +2,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main ()
+{
+    string baris;
+
+    // membuka file dalam mode menulis.
+    ofstream outfile;
+    // menunjuk ke sebuah nama file
+    outfile.open("ContohFile.txt");
+
+    cout << ">= Menulis File, \'q\' untuk keluar" << endl;
+
+}
