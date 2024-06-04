8edaefdedf0dc60d3b4c020ff26b8c7867f89ec0
akmaalsyh <taufansyah50@gmail.com>
2024-06-04 14:48:53 +0700

    memasukkan perintah menunjuk ke sebuah file

--- iofile.cpp
+++ iofile.cpp
@@ -25,10 +25,13 @@ int main ()
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
-    
+
     // selesai dalam menulis sekarang tutup filenya
     outfile.close();
 
     // membuka file dalam mode membaca
     ifstream infile;
+    // menunjuk ke sebuah file
+    infile.open("ContohFile.txt");
+    
 }
